e98409e46353e7fbddb8ebdb761f15ea642f970b
Alexandru Tenche <alex.tenche@gmail.com>
2021-11-07 12:21:42 +0200

    reformat

--- src/test/java/mflix/api/daos/FacetedSearchTest.java
+++ src/test/java/mflix/api/daos/FacetedSearchTest.java
@@ -24,96 +24,97 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class FacetedSearchTest extends TicketTest {
 
-  private MovieDao dao;
-  @Autowired MongoClient mongoClient;
+    private MovieDao dao;
+    @Autowired
+    MongoClient mongoClient;
 
-  @Value("${spring.mongodb.database}")
-  String databaseName;
+    @Value("${spring.mongodb.database}")
+    String databaseName;
 
-  @Before
-  public void setup() {
-    this.dao = new MovieDao(mongoClient, databaseName);
-  }
+    @Before
+    public void setup() {
+        this.dao = new MovieDao(mongoClient, databaseName);
+    }
 
-  @Test
-  public void testRatingRuntimeBuckets() {
+    @Test
+    public void testRatingRuntimeBuckets() {
 
-    String cast = "Salma Hayek";
+        String cast = "Salma Hayek";
 
-    List<Document> moviesInfo = dao.getMoviesCastFaceted(20, 0, cast);
+        List<Document> moviesInfo = dao.getMoviesCastFaceted(20, 0, cast);
 
-    ArrayList<Document> allMovies = (ArrayList<Document>) moviesInfo.get(0).get("movies");
-    assertEquals(
-        "Check your movies sub-pipeline on getMoviesFaceted() for multiple cast in single cast",
-        20,
-        allMovies.size());
+        ArrayList<Document> allMovies = (ArrayList<Document>) moviesInfo.get(0).get("movies");
+        assertEquals(
+                "Check your movies sub-pipeline on getMoviesFaceted() for multiple cast in single cast",
+                20,
+                allMovies.size());
 
-    ArrayList rating = (ArrayList<Document>) moviesInfo.get(0).get("rating");
-    assertEquals(
-        "Check your $bucket rating sub-pipeline on getMoviesFaceted() for multiple cast in single cast",
-        3,
-        rating.size());
+        ArrayList rating = (ArrayList<Document>) moviesInfo.get(0).get("rating");
+        assertEquals(
+                "Check your $bucket rating sub-pipeline on getMoviesFaceted() for multiple cast in single cast",
+                3,
+                rating.size());
 
-    ArrayList runtime = (ArrayList<Document>) moviesInfo.get(0).get("runtime");
-    assertEquals(
-        "Check your $bucket runtime sub-pipeline on getMoviesFaceted() for multiple cast in single cast",
-        3,
-        runtime.size());
-  }
+        ArrayList runtime = (ArrayList<Document>) moviesInfo.get(0).get("runtime");
+        assertEquals(
+                "Check your $bucket runtime sub-pipeline on getMoviesFaceted() for multiple cast in single cast",
+                3,
+                runtime.size());
+    }
 
-  @Test
-  public void testFacetedSearchPaging() {
+    @Test
+    public void testFacetedSearchPaging() {
 
-    String cast = "Paul Newman";
+        String cast = "Paul Newman";
 
-    List<Document> moviesInfo = dao.getMoviesCastFaceted(20, 2 * 20, cast);
+        List<Document> moviesInfo = dao.getMoviesCastFaceted(20, 2 * 20, cast);
 
-    assertEquals("Should return a list of one faceted document", 1, moviesInfo.size());
+        assertEquals("Should return a list of one faceted document", 1, moviesInfo.size());
 
-    ArrayList movies = (ArrayList<Document>) moviesInfo.get(0).get("movies");
-    assertEquals(
-        "Check your movies sub-pipeline on getMoviesFaceted() for multiple cast in paged results",
-        4,
-        movies.size());
+        ArrayList movies = (ArrayList<Document>) moviesInfo.get(0).get("movies");
+        assertEquals(
+                "Check your movies sub-pipeline on getMoviesFaceted() for multiple cast in paged results",
+                4,
+                movies.size());
 
-    ArrayList rating = (ArrayList<Document>) moviesInfo.get(0).get("rating");
-    assertEquals(
-        "Check your $bucket rating sub-pipeline on getMoviesFaceted() in multiple cast in paged results",
-        1,
-        rating.size());
+        ArrayList rating = (ArrayList<Document>) moviesInfo.get(0).get("rating");
+        assertEquals(
+                "Check your $bucket rating sub-pipeline on getMoviesFaceted() in multiple cast in paged results",
+                1,
+                rating.size());
 
-    ArrayList runtime = (ArrayList<Document>) moviesInfo.get(0).get("runtime");
-    assertEquals(
-        "Check your $bucket runtime sub-pipeline on getMoviesFaceted() in paged results",
-        3,
-        runtime.size());
-  }
+        ArrayList runtime = (ArrayList<Document>) moviesInfo.get(0).get("runtime");
+        assertEquals(
+                "Check your $bucket runtime sub-pipeline on getMoviesFaceted() in paged results",
+                3,
+                runtime.size());
+    }
 
-  @Test
-  public void testFacetedMultipleCast() {
+    @Test
+    public void testFacetedMultipleCast() {
 
-    String[] cast = {"Brad Pitt", "Angelina Jolie"};
+        String[] cast = {"Brad Pitt", "Angelina Jolie"};
 
-    List<Document> moviesInfo = dao.getMoviesCastFaceted(20, 2 * 20, cast);
+        List<Document> moviesInfo = dao.getMoviesCastFaceted(20, 2 * 20, cast);
 
-    assertEquals("Should return a list of one faceted document", 1, moviesInfo.size());
+        assertEquals("Should return a list of one faceted document", 1, moviesInfo.size());
 
-    ArrayList movies = (ArrayList<Document>) moviesInfo.get(0).get("movies");
-    assertEquals(
-        "Check your movies sub-pipeline on getMoviesFaceted() in multiple cast for multiple cast",
-        16,
-        movies.size());
+        ArrayList movies = (ArrayList<Document>) moviesInfo.get(0).get("movies");
+        assertEquals(
+                "Check your movies sub-pipeline on getMoviesFaceted() in multiple cast for multiple cast",
+                16,
+                movies.size());
 
-    ArrayList rating = (ArrayList<Document>) moviesInfo.get(0).get("rating");
-    assertEquals(
-        "Check your $bucket rating sub-pipeline on getMoviesFaceted() in multiple cast",
-        3,
-        rating.size());
+        ArrayList rating = (ArrayList<Document>) moviesInfo.get(0).get("rating");
+        assertEquals(
+                "Check your $bucket rating sub-pipeline on getMoviesFaceted() in multiple cast",
+                3,
+                rating.size());
 
-    ArrayList runtime = (ArrayList<Document>) moviesInfo.get(0).get("runtime");
-    assertEquals(
-        "Check your $bucket runtime sub-pipeline on getMoviesFaceted() in multiple cast",
-        5,
-        runtime.size());
-  }
+        ArrayList runtime = (ArrayList<Document>) moviesInfo.get(0).get("runtime");
+        assertEquals(
+                "Check your $bucket runtime sub-pipeline on getMoviesFaceted() in multiple cast",
+                5,
+                runtime.size());
+    }
 }
--- src/test/java/mflix/api/daos/TimeoutsTest.java
+++ src/test/java/mflix/api/daos/TimeoutsTest.java
@@ -25,39 +25,40 @@ import java.util.concurrent.TimeUnit;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TimeoutsTest extends TicketTest {
 
-  @Autowired MongoClient mongoClient;
-  private String mongoUri;
+    @Autowired
+    MongoClient mongoClient;
+    private String mongoUri;
 
-  @Value("${spring.mongodb.database}")
-  String databaseName;
+    @Value("${spring.mongodb.database}")
+    String databaseName;
 
-  private MovieDao movieDao;
+    private MovieDao movieDao;
 
-  @Before
-  public void setUp() throws IOException {
-    this.movieDao = new MovieDao(mongoClient, databaseName);
-    mongoUri = getProperty("spring.mongodb.uri");
-    mongoClient = MongoClients.create(mongoUri);
-  }
+    @Before
+    public void setUp() throws IOException {
+        this.movieDao = new MovieDao(mongoClient, databaseName);
+        mongoUri = getProperty("spring.mongodb.uri");
+        mongoClient = MongoClients.create(mongoUri);
+    }
 
-  @Test
-  public void testConfiguredWtimeout() {
-    WriteConcern wc =
-            this.movieDao.mongoClient.getDatabase(databaseName).getWriteConcern();
+    @Test
+    public void testConfiguredWtimeout() {
+        WriteConcern wc =
+                this.movieDao.mongoClient.getDatabase(databaseName).getWriteConcern();
 
-    Assert.assertNotNull(wc);
-    int actual = wc.getWTimeout(TimeUnit.MILLISECONDS);
-    int expected = 2500;
-    Assert.assertEquals("Configured `wtimeoutms` not set has expected", expected, actual);
-  }
+        Assert.assertNotNull(wc);
+        int actual = wc.getWTimeout(TimeUnit.MILLISECONDS);
+        int expected = 2500;
+        Assert.assertEquals("Configured `wtimeoutms` not set has expected", expected, actual);
+    }
 
-  @Test
-  public void testConfiguredConnectionTimeoutMs() {
-    ConnectionString connectionString = new ConnectionString(mongoUri);
-    int expected = 2000;
-    int actual = connectionString.getConnectTimeout();
+    @Test
+    public void testConfiguredConnectionTimeoutMs() {
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        int expected = 2000;
+        int actual = connectionString.getConnectTimeout();
 
-    Assert.assertEquals(
-        "Configured `connectionTimeoutMS` does not match expected", expected, actual);
-  }
+        Assert.assertEquals(
+                "Configured `connectionTimeoutMS` does not match expected", expected, actual);
+    }
 }
